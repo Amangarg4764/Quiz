d3c850a8ae7f49a849be9c4c098a6ff728a7c8c1
Aman garg <aman0944.cse19@chitkara.edu.in>
2023-03-11 23:40:53 +0530

    comment on 1 line of moongose file to run in host onrender

--- Config/mongoose.js
+++ Config/mongoose.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-const mongoose=require('mongoose');
+//require('dotenv').config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || `mongodb://127.0.0.1:27017/onlinequiz`);
+mongoose.connect(
+  process.env.MONGODB_URL || `mongodb://127.0.0.1:27017/onlinequiz`
+);
 
-const db=mongoose.connection;
+const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'error in database connection'));
+db.on("error", console.error.bind(console, "error in database connection"));
 
-db.once('open',function(){
-    console.log("Successful Connected to datbase");
+db.once("open", function () {
+  console.log("Successful Connected to datbase");
 });
